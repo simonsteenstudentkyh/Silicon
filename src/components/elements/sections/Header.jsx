e73d4b6c9977b0aa2a03b29936c7c6a5317c3af6
simonsteenstudentkyh <simon.steen@student.kyh.se>
2025-03-16 12:39:25 +0100

    Header + styling

--- src/components/elements/sections/Header.jsx
+++ src/components/elements/sections/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { DarkModeContext } from '../../../contexts/DarkModeContext';
 
 const Header = () => {
+  const {darkMode, toggleDarkMode } = useContext(DarkModeContext);
+
   return (
     <header>
-      Header
+      <Link to="/" className="navigation-logo">
+        <img src={ darkMode ? "/images/logotype_dark.svg" : "/images/logotype_light.svg" } alt="Silicon Inc." />
+      </Link>
+
+      <nav className="main-navigation">
+        <NavLink to="/features" className="navigation-link">Features</NavLink>
+        <NavLink to="/contacts" className="navigation-link">Contacts</NavLink>
+      </nav>
+
+      <div id="dark-mode">
+        Dark Mode
+        <input className="switch" type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
+      </div>
+
+      <button className="btn primary small round">
+        <i className="bi bi-person"/>
+        Sign in / up
+      </button>
     </header>
   )
 }
