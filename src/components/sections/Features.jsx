197557a07951dee29ff132d86bddc57b1bf64fe9
simonsteenstudentkyh <simon.steen@student.kyh.se>
2025-03-16 20:40:27 +0100

    Features section

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -3,10 +3,56 @@ import React from 'react'
 const Features = () => {
   return (
     <section id="features-section">
-      <img src="images/features-image.svg" alt="Features" />
+      <img className="image" src="images/features-image.svg" alt="Features" />
+
+      <div className="title">
+        <h1>App Features</h1>
+        <p className="large">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat mollis egestas. Nam luctus facilisis ultrices. Pellentesque volutpat ligula est. Mattis fermentum, at nec lacus.</p>
+      </div>
 
       <div className="features">
-        
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/credit-card.svg" alt="Credit Card" />
+          </div>
+          <h5>Easy Payments</h5>
+          <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
+        </div>
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/shield.svg" alt="Shield" />
+          </div>
+          <h5>Data Security</h5>
+          <p>Augue pulvinar justo, fermentum fames aliquam accumsan vestibulum non.</p>
+        </div>
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/bars-graphic.svg" alt="Bars Graphic" />
+          </div>
+          <h5>Cost Statistics</h5>
+          <p>Mattis urna ultricies non amet, purus in auctor non. Odio vulputate ac nibh.</p>
+        </div>
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/communication.svg" alt="Communication" />
+          </div>
+          <h5>Support 24/7</h5>
+          <p>A elementum, imperdiet enim, pretium etiam facilisi in aenean quam mauris.</p>
+        </div>
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/wallet.svg" alt="Wallet" />
+          </div>
+          <h5>Regular Cashback</h5>
+          <p>Sit facilisis dolor arcu, fermentum vestibulum arcu elementum imperdiet eleifend.</p>
+        </div>
+        <div className="feature">
+          <div className="feature-icon">
+            <img src="/images/icons/happy.svg" alt="Credit Card" />
+          </div>
+          <h5>Top Standards</h5>
+          <p>Faucibus cursus maecenas lorem cursus nibh. Sociis sit risus id. Sit facilisis dolor arcu.</p>
+        </div>
       </div>
     </section>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/sections/Hero'
 import Brands from '../components/sections/Brands'
 import Testimonials from '../components/sections/Testimonials'
+import Features from '../components/sections/Features'
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Brands />
+      <Features />
       <Testimonials />
     </>
   )
