3c7f3ccd847ec0bd2fce81d4c4fd6e864f3fd9ef
simonsteenstudentkyh <simon.steen@student.kyh.se>
2025-03-16 17:34:42 +0100

    Hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/elements/sections/Hero'
 import Brands from '../components/elements/sections/Brands'
 
 const HomePage = () => {
   return (
     <>
+      <Hero />
       <Brands />
     </>
   )
